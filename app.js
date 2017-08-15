ce41f0df6f98eec09d611fe0ec2d75bc6161df90
Shannon <reidy.pf@gmail.com>
2017-08-15 15:09:32 -0500

    completed

--- app.js
+++ app.js
@@ -8,9 +8,57 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+let todoList = {
+  title: 'To Do List',
+  items: [{
+      'text': "Learn Node basics",
+      'done': true
+    },
+    {
+      'text': "Learn Express basics",
+      'done': true
+    },
+    {
+      'text': "Learn Mustache",
+      'done': true
+    },
+    {
+      'text': "Learn HTML forms with Express",
+      'done': false
+    },
+    {
+      'text': "Learn about authentication",
+      'done': false
+    },
+    {
+      'text': "Learn how to connect to PostgreSQL",
+      'done': false
+    },
+    {
+      'text': "Learn how to create databases",
+      'done': false
+    },
+    {
+      'text': "Learn how to create databases",
+      'done': false
+    },
+    {
+      'text': "Learn SQL",
+      'done': false
+    },
+    {
+      'text': "Learn how to connect to PostgreSQL from Node",
+      'done': false
+    },
+    {
+      'text': "Learn how to use Sequelize",
+      'done': false
+    }
+  ]
+}
 //Listening on root
 app.get('/todo/', function (req, res) {
-  // TODO write your code here
+  res.render('todo', todoList)
 })
 
 app.listen(3000, function () {
